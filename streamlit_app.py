df217a9c7c915657eec535ef41f2ea2980225433
sudisy <77107097+sudisy@users.noreply.github.com>
2024-12-30 18:16:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import requests
 
 
-
-st.write("Hello "+ st.experimental_user.email)
+email = st.experimental_user.email
+st.write("Hello "+ str(email))
 
 x = st.slider("Select a value")
 st.write(x,"squared is<img src=x>", x * x)
